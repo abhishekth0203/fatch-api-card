1c03adf77204621a22ee2769aa15473000845980
ABHISHEK KUMAR THAKUR <121752846+abhishekth0203@users.noreply.github.com>
2023-01-20 23:13:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let section = document.querySelector("section");
+fatch("https://api.github.com/users")
+.then(res => {
+    res
+    .json()
+    .then(data => {
+        data.map(value => {
+            let { login, avatar_url, html_url } = value;
+            section.innerHTML += `
+            <div>
+            <img src=${avatar_url} alt=${login} width="200px">
+            <h1> ${login} <h1>
+            <a href=${html_url} target="_blank"> Goto Github profile</a>
+            </div>
+            `;
+        });
+    })
+    .catch(err => console.log(err));
+})
+
+.catch(err => console.log(err))
+.finally(() => {
+    console.log("server respounded....");
+});
+
